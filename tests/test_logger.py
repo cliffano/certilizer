f66e9468a1d042996dafa9b2f1ac75a6b62c191b
Cliffano Subagio <cliffano@gmail.com>
2025-06-07 23:23:40 +1000

    Add unit test for logger.

---
+++ tests/test_logger.py
@@ -0,0 +1,12 @@
+# pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,duplicate-code,too-many-locals
+import logging
+import unittest
+from certilizer.logger import init
+
+
+class TesLogger(unittest.TestCase):
+
+    def test_init(self):
+
+        logger = init()
+        assert isinstance(logger, logging.LoggerAdapter) is True
